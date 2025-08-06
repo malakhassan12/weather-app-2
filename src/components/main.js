f080012e7ef805a79ff75b17b9e8b3ab046d9d7b
malakhassan12 <158052165+malakhassan12@users.noreply.github.com>
2025-08-07 00:34:21 +0300

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -95,7 +95,7 @@ export default function Main() {
         >
           <Item sx={{ backgroundColor: "#202b3b" }}>
             <div>
-             <img src={`${process.env.PUBLIC_URL}/images/blue-umbrella.png`} style={{ width: "100%", height: "100%" }} />
+<img src="./images/blue-umbrella.png" style={{ width: "100%", height: "100%" }} />
 
             </div>
           </Item>
